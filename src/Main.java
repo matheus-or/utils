717672036865ba79b136c848f75922966ec7355e
matheus-or <matheusolivvee@gmail.com>
2022-05-01 22:43:31 -0300

    Troca de nome de variavel

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
         System.out.println("Hello World!");
     }
 
-    private String formatMascaraCpfCnpj(String descRetornoDocSujPass) {
+    private String formatMascaraCpfCnpj(String cpfCnpj) {
         String cpfCnpjFormatado;
-        if (descRetornoDocSujPass.length() > 11) {
-            cpfCnpjFormatado = descRetornoDocSujPass.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})", "$1.$2.$3/$4-$5");
-        } else if (descRetornoDocSujPass.length() == 11) {
-            cpfCnpjFormatado = descRetornoDocSujPass.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+        if (cpfCnpj.length() > 11) {
+            cpfCnpjFormatado = cpfCnpj.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})", "$1.$2.$3/$4-$5");
+        } else if (cpfCnpj.length() == 11) {
+            cpfCnpjFormatado = cpfCnpj.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
         } else {
-            return descRetornoDocSujPass;
+            return cpfCnpj;
         }
 
         return cpfCnpjFormatado;
